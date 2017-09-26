c103e916c4c210882e7b15cf0f34a37a19468fc3
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-09-26 08:46:47 -0500

    Update RockPaperScissors.java

--- Chapter5/src/gameZone/RockPaperScissors.java
+++ Chapter5/src/gameZone/RockPaperScissors.java
@@ -1,10 +1,74 @@
 package gameZone;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class RockPaperScissors {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Random rand = new Random();
+		Scanner input = new Scanner(System.in);
+		int choiceRPS;
+		int compChoice = rand.nextInt(3)+1;
+		
+		
+		System.out.println("Lets play Rock, Paper, Scissors!");
+		System.out.println("Choose 1 for Rock, 2 for Paper, or 3 for Scissors (The computer will follow the same rules) >> ");
+		choiceRPS = input.nextInt();
+		
+		if(choiceRPS == 1)
+		{
+			if(compChoice == 1)
+			{
+				System.out.println("It's a tie. The computer chose " + compChoice);
+			}
+			if(compChoice == 2)
+			{
+				System.out.println("You lose! HaHa! The computer chose " + compChoice);
+			}
+			if(compChoice == 3)
+			{
+				System.out.println("You win! Good Job! The computer chose " + compChoice);
+			}
+			
+		}
+		
+		if(choiceRPS == 2)
+		{
+			if(compChoice == 2)
+			{
+				System.out.println("It's a tie. The computer chose " + compChoice);
+			}
+			if(compChoice == 3)
+			{
+				System.out.println("You lose! HaHa! The computer chose " + compChoice);
+			}
+			if(compChoice == 1)
+			{
+				System.out.println("You win! Good Job! The computer chose " + compChoice);
+			}
+			
+		}
+		
+		if(choiceRPS == 3)
+		{
+			if(compChoice == 3)
+			{
+				System.out.println("It's a tie. The computer chose " + compChoice);
+			}
+			if(compChoice == 1)
+			{
+				System.out.println("You lose! HaHa! The computer chose " + compChoice);
+			}
+			if(compChoice == 2)
+			{
+				System.out.println("You win! Good Job! The computer chose " + compChoice);
+			}
+			
+		}
+		
+		
 	}
 
 }
